54ee12303809921dd9b17990f8f15c797fde60a3
mugicha123 <mugicha0ryokucha@gmail.com>
2026-08-10 17:06:33 +0900

    コードを関数に変換

--- content.js
+++ content.js
@@ -1,25 +1,27 @@
-//LockDivを作成
-const LockDiv = document.createElement('div');
-//html,bodyを取得
-const html = document.querySelector("html");
-const body = document.querySelector("body");
-//html,bodyを100%のサイズに設定
-html.style.width = "100%";
-html.style.height = "100%";
-body.style.width = "100%";
-body.style.height = "100%";
-//余白を削除
-const style = document.createElement('style');
-style.textContent = '* { margin: 0; padding: 0; }';
-document.head.appendChild(style);
-//LockDivのスタイルを設定
-LockDiv.style.position = "fixed";
-LockDiv.style.bottom = "20px";
-LockDiv.style.width = "100%";
-LockDiv.style.height = "100%";
-LockDiv.style.top = '0';
-LockDiv.style.left = '0';
-LockDiv.style.zIndex = '9999';
-LockDiv.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-//LockDivをbodyに追加
-document.body.appendChild(LockDiv);
+function lockPage() {
+    //LockDivを作成
+    const lockDiv = document.createElement('div');
+    //html,bodyを取得
+    const html = document.querySelector("html");
+    const body = document.querySelector("body");
+    //html,bodyを100%のサイズに設定
+    html.style.width = "100%";
+    html.style.height = "100%";
+    body.style.width = "100%";
+    body.style.height = "100%";
+    //余白を削除
+    const style = document.createElement('style');
+    style.textContent = '* { margin: 0; padding: 0; }';
+    document.head.appendChild(style);
+    //LockDivのスタイルを設定
+    lockDiv.style.position = "fixed";
+    lockDiv.style.bottom = "20px";
+    lockDiv.style.width = "100%";
+    lockDiv.style.height = "100%";
+    lockDiv.style.top = '0';
+    lockDiv.style.left = '0';
+    lockDiv.style.zIndex = '9999';
+    lockDiv.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    //LockDivをbodyに追加
+    document.body.appendChild(lockDiv);
+}
